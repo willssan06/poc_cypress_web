df31b803a20f7bbeb64fac4924b459bf9c6a1b21
Wilson Barboza <wilson.barbozasilvasantos@thomsonreuters.com>
2024-10-10 16:54:31 -0300

    First commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,55 @@
+/// <reference types="cypress"/>
+
+describe('Login', () => {
+    
+    it('Login com sucesso', () => {
+
+        // Dado 
+        cy.visit('https://automationpratice.com.br/login')
+
+        cy.get('#user').type('edu@qazando.com')
+
+        cy.get('#password').type('123456')
+
+        // Quando
+
+        cy.get('#btnLogin').click()
+
+        // Então
+
+        cy.get('#swal2-title').should('have.text', "Login realizado")
+        cy.url().should('contain', '/my-account')
+    })
+    it('Login com email vazio', () => {
+
+        cy.visit('https://automationpratice.com.br/login')
+        cy.get('#password').type('123456')
+        cy.get('#btnLogin').click()
+        cy.get('.invalid_input').should('have.text', "E-mail inválido.")
+    })
+    it('Login com senha vazia', () => {
+
+        cy.visit('https://automationpratice.com.br/login')
+        cy.get('#user').type('edu@qazando.com')
+        cy.get('#btnLogin').click()
+        cy.get('.invalid_input').should('have.text', "Senha inválida.")
+
+    })
+    it('Login com email invalido', () => {
+
+        cy.visit('https://automationpratice.com.br/login')
+        cy.get('#user').type('akljljljljlkjk')
+        cy.get('#password').type('123456')
+        cy.get('#btnLogin').click()
+        cy.get('.invalid_input').should('have.text', "E-mail inválido.")
+
+    })
+    it('Login com senha invalida', () => {
+        cy.visit('https://automationpratice.com.br/login')
+        cy.get('#user').type('edu@qazando.com')
+        cy.get('#password').type('123')
+        cy.get('#btnLogin').click()
+        cy.get('.invalid_input').should('have.text', "Senha inválida.")
+    })
+})
+// automação web cypress
