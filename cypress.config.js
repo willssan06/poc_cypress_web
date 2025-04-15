57ea28fbda326b3e9d3e5f40ee7d74424b5b3bed
Wilson Barboza Silva Santos <willssan06@gmail.com>
2025-04-15 12:01:34 -0300

    apresentação_poc

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,24 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // eventos podem ser configurados aqui se precisar
     },
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.js",
+  },
+
+  screenshotsFolder: "cypress/screenshots",
+  videosFolder: "cypress/videos",
+
+  video: true,
+  videoCompression: 32,
+  videoUploadOnPasses: false, // grava vídeo apenas se o teste falhar
+
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true,
   },
 });
