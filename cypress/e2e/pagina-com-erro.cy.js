a0f79916b3cc15c6987cfd29a9c22dd6e2058262
Wilson Barboza Silva Santos <willssan06@gmail.com>
2025-04-15 13:52:16 -0300

    Automação CI

--- cypress/e2e/pagina-com-erro.cy.js
+++ cypress/e2e/pagina-com-erro.cy.js
@@ -1,12 +1,12 @@
-describe('Teste de Exemplo com Falha', () => {
-    it('Página incorreta', () => {
-      cy.visit('https://automationpratice.com.br/login');
+// describe('Teste de Exemplo com Falha', () => {
+//     it('Página incorreta', () => {
+//       cy.visit('https://automationpratice.com.br/login');
       
-      // Aguarda um pouco só pra capturar bem no vídeo
-      cy.wait(1000);
+//       // Aguarda um pouco só pra capturar bem no vídeo
+//       cy.wait(1000);
       
-      // Verifica o título (intencionalmente errado para forçar falha)
-      cy.title().should('include', 'QAZANDO simulação erro'); // Isso vai falhar de propósito
-    });
-  });
+//       // Verifica o título (intencionalmente errado para forçar falha)
+//       cy.title().should('include', 'QAZANDO simulação erro'); // Isso vai falhar de propósito
+//     });
+//   });
   
